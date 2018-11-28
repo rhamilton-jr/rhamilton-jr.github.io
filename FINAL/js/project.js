118c72205fa9676da8cae76b3cb293d8f443b66e
Robert Hamilton <robertwhamilton@gmail.com>
2018-11-27 22:09:37 -0500

    FINAL

--- FINAL/js/project.js
+++ FINAL/js/project.js
@@ -11,3 +11,21 @@ function showheaderhideheader() {
 }
 
 $('#hamburgermenu').click(showheaderhideheader)
+
+/* const cats = [
+  "../img/drainweb.png",
+  "../img/deerweb.png"
+]
+
+const node = document.getElementById("firstheroslider");
+
+const cycleImages = (images, container, step) => {
+    images.forEach((image, index) => (
+    setTimeout(() => {
+        container.style.backgroundImage = `url(${image})`  
+    }, step * (index + 1))
+  ))
+  setTimeout(() => cycleImages(images, container, step), step * images.length)
+}
+
+cycleImages(cats, node, 1000) */
